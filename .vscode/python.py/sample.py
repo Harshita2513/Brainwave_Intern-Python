d0319c4961d9394a8b916777ca463ba1c2e836b3
Harshita2513 <harshitaupadhyay3152006@gmail.com>
2025-02-25 18:19:08 +0530

    rough work

---
+++ .vscode/python.py/sample.py
@@ -0,0 +1,5 @@
+ghbjhj
+https://github.com/Harshita2513/Brainwave_Intern-Python
+ATM_Interface.py
+https://github.com/Harshita2513/Brainwave_Intern-Python/blob/120b32a5b85097010425c3910dbe03abf8f3fe8f/ATM_Interface.py
+https://github.com/Harshita2513/Brainwave_Intern-Python/blob/main/ATM_Interface.py
